7a266f3a353635ce1b155909247f63cbffdeb010
z-bit <guenther@z-bit.co.uk>
2016-12-17 15:20:44 +0000

    up to video 03

---
+++ vid03.js
@@ -0,0 +1,40 @@
+// vid02.js
+'use strict';
+
+const {graphql, buildSchema} = require('graphql');
+
+const schema = buildSchema(`
+    type  Query {
+        id: ID,
+        title: String,
+        duration: Int,
+        watched: Boolean
+    }
+    type Schema {
+        query: Query
+    }
+`);
+
+const resolvers = {
+    id: () => '1',
+    title: () => 'bar',
+    duration: () => 180,
+    watched: true
+};
+const query = `
+    query myFirstQuery {
+        id
+        title
+        duration
+        watched
+    }
+`;
+
+graphql(schema, query, resolvers)
+    .then( (result) => {console.log(result)} )
+    .catch( (error) => {console.log(error)} )
+;
+
+// call:        node vid02
+// returns:     { data: { id: '1', title: 'bar', duration: 180, watched: true } }
+
