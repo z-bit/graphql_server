d84f28971bdcd638321a677d4e8b2a53f8039596
z-bit <guenther@z-bit.co.uk>
2016-12-18 17:28:59 +0000

    up to vid07

---
+++ data/index.js
@@ -0,0 +1,28 @@
+// /data/index.js
+
+const videoA = {
+    id: 'a',
+    title: 'Create a GraphQl Schema',
+    duration: 120,
+    watched: true
+};
+
+const videoB = {
+    id: 'b',
+    title: 'Ember.js CLI',
+    duration: 240,
+    watched: false
+};
+
+const videos = [videoA, videoB];
+
+const getVideoById = (id) => new Promise( (resolve) => {
+    const [video] = videos.filter( (video) => {
+        return video.id === id;
+    });
+    resolve(video);
+});
+
+exports.getVideoById = getVideoById;
+
+
---
+++ vid07.js
@@ -0,0 +1,67 @@
+// vid07.js
+'use strict';
+
+const express  = require('express');
+const graphqlHTTP  = require("express-graphql");
+const {
+    GraphQLSchema,
+    GraphQLObjectType,
+    GraphQLID,
+    GraphQLString,
+    GraphQLInt,
+    GraphQLBoolean
+} = require('graphql');
+const PORT = process.env.port || 3000;
+const server = express();
+const { getVideoById } = require('./data');
+
+const videoType = new GraphQLObjectType({
+    name: 'Video',
+    description: 'A video on egghead.io',
+    fields: {
+        id: {type: GraphQLID, description: 'The id of the video.'},
+        title: {type: GraphQLString, description: 'The title of the video.'},
+        duration: {type: GraphQLInt, description: 'the duration of the vodeo (in seconds).'},
+        watched: {type: GraphQLBoolean, description: 'Whether or not the viewer has watched the video.'}
+    }
+});
+
+const queryType = new GraphQLObjectType({
+    name: 'queryType',
+    description: 'The root query type.',
+    fields: {
+        video: {
+            type: videoType,
+            args: {
+                id: {
+                    type: GraphQLID,
+                    description: 'The id of the video.'
+                }
+            },
+            resolve: (_, args) => {
+                return getVideoById(args.id);
+            }
+
+        }
+    }
+});
+
+const schema = new GraphQLSchema({
+    query: queryType
+});
+
+server.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: true
+}));
+
+server.listen(PORT, () => {
+    console.log(`Listening on http://localhost:${PORT}`);
+});
+
+// call:        node vid07
+// returns:     Listening on http://localhost:3000
+
+// open in browser with http://localhost:3000/graphql
+
+
